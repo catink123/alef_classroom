c9c05f4db474ef734be2cddbe56a7f9229daf97e
Catink123 <qwerty.qaz120@gmail.com>
2025-11-28 02:07:58 +0100

    fix: uploaded media not being served in production

--- alef_classroom/core/urls.py
+++ alef_classroom/core/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.views.static import serve
 from .views import (
     HomeView, dashboard_view, 
     dashboard_classroom_stats, dashboard_enrolled_stats,
@@ -62,3 +63,8 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Serve media files in production (use a web server like nginx in real production)
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
